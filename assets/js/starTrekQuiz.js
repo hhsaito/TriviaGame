881d22c5879b4b13dccb30ea007720681a3187ae
hhsaito <hhsaito@users.noreply.github.com>
2016-11-04 23:41:15 -0500

    added comment to js.

--- assets/js/starTrekQuiz.js
+++ assets/js/starTrekQuiz.js
@@ -45,10 +45,12 @@ $(document).ready(function () {
 	});
 
 	$(document).on('click', '.quiz-choice', function() {
+        // Get selected answer and its number
 		var clicked = $(this),
 		 userAnswer = clicked.prevAll().length;
 
 		if ( userAnswer === quiz[current].correctAnswer ) {
+            // add to correct score
             score++;
             $('#quiz-question').html('<p>You are correct.</p><p>' + quiz[current].tidbits + '</p><img src="assets/images/' + quiz[current].resultImage + '"/>');
             $('#choices').empty();
@@ -57,7 +59,7 @@ $(document).ready(function () {
             setTimeout(continueGame, 7000);
 		}
 		else {
-			console.log( 'incorrect' );
+            // add to incorrect
             incorrect++;
             $('#quiz-question').html('<p>Sorry, that is not correct.</p><p>' + quiz[current].tidbits + '</p><img src="assets/images/' + quiz[current].resultImage + '"/>');
             $('#choices').empty();
@@ -66,6 +68,7 @@ $(document).ready(function () {
 		}
 	});
     $(document).on('click', '#restart', function() {
+        // reset variable and display first question
         counter = 0,
         score = 0,
         current = 0,
@@ -77,14 +80,16 @@ $(document).ready(function () {
 });
 
 function displayQuestion() {
-    console.log(current);
+    // find current question
 	var question = quiz[current].question;
     $('#choices').empty();
 
+    // inital timer
     $('#remaining').html('<p>Time remaining: 30</p>');
+    // get question
     $('#quiz-question').html('<p>' + question + '</p>');
-    console.log(quiz[current].answers.length);
 
+    // loop through array to get questions and append to ul
     for ( var i = 0; i < quiz[current].answers.length; i++ ) {
         var choice = quiz[current].answers[i];
         $('<li class="quiz-choice"><a href="#">' + choice + '</a></li>').appendTo('#choices');
@@ -92,45 +97,46 @@ function displayQuestion() {
 }
 
 function continueGame () {
+    // next item in the array
     current++;
-    
+    // items still in array
     if ( current < quiz.length ) {
         timer();
         displayQuestion();
     }
+    // last item in array
     else {
-
         endGame();
     }
 }
 
 function endGame() {
-    //$('#remaining').empty();
+    // Display scores
 	$('#quiz-question').html('<h2>Here is how you did.</h2><p><strong>Correct Answers: </strong>' + score + '</p><p><strong>Incorrect Answers: </strong>' + incorrect + '</p><p><strong>Unanswered: </strong>' + unanswered + '</p><button id="restart">Restart</button>');
 	$('#choices').empty();
     
     clearInterval(counter);
 }
 function timer() {
+    // set timer to 30 and interval for 1 second
     seconds = 30;
     counter = setInterval(decrement, 1000);
 }
 function decrement(){
     // Decrease number by one.
     seconds--;
-    // Show the number in the #show-number tag.
+    // Show the number in the #remaining tag.
     $('#remaining').html('<p>Time remaining: ' + seconds + '</p>');
 
-    // Once number hits zero...
+    // Once number hits zero and there is another item in the array
     if ( seconds == 0 && quiz[current+1] ) {
-
         $('#quiz-question').html('<p>Sorry. Time is up</p><p>' + quiz[current].tidbits + '</p><img src="assets/images/' + quiz[current].resultImage + '"/>');
         $('#choices').empty();
         clearInterval(counter);
         unanswered++;
         setTimeout(continueGame, 7000);
-
     }
+    // last item in the array
     else if ( seconds == 0 ) {
         $('#quiz-question').html('<p>Sorry. Time is up</p><p>' + quiz[current].tidbits + '</p><img src="assets/images/' + quiz[current].resultImage + '"/>');
         $('#choices').empty();
